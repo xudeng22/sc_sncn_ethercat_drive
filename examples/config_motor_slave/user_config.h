ad1df592c44a4a9a8b235cf27445f8b09af65c13
Frank Jeschke <fjeschke@synapticon.com>
2016-06-09 19:45:03 +0200

    Change default configuration values
    These values are used for the DC1K Mabi test montor.

--- examples/config_motor_slave/user_config.h
+++ examples/config_motor_slave/user_config.h
@@ -27,23 +27,23 @@
 #define MOTOR_TYPE  BLDC_MOTOR
 
 // NUMBER OF POLE PAIRS (if applicable)
-#define POLE_PAIRS  3
+#define POLE_PAIRS  2
 
 // WINDING TYPE (if applicable) [STAR_WINDING, DELTA_WINDING]
 #define BLDC_WINDING_TYPE   STAR_WINDING
 
 // MOTOR POLARITY [NORMAL_POLARITY, INVERTED_POLARITY]
-#define MOTOR_POLARITY      INVERTED_POLARITY
+#define MOTOR_POLARITY      NORMAL_POLARITY
 
 /////////////////////////////////////////////
 //////  MOTOR SENSORS CONFIGURATION
 ///////////////////////////////////////////
 
 // SENSOR USED FOR COMMUTATION (if applicable) [HALL_SENSOR]
-#define MOTOR_COMMUTATION_SENSOR   HALL_SENSOR
+#define MOTOR_COMMUTATION_SENSOR   BISS_SENSOR
 
 // SENSOR USED FOR CONTROL FEEDBACK [HALL_SENSOR, QEI_SENSOR, BISS_SENSOR]
-#define MOTOR_FEEDBACK_SENSOR      HALL_SENSOR
+#define MOTOR_FEEDBACK_SENSOR      BISS_SENSOR
 
 // TYPE OF INCREMENTAL ENCODER (if applicable) [QEI_WITH_INDEX, QEI_WITH_NO_INDEX]
 #define QEI_SENSOR_INDEX_TYPE      QEI_WITH_INDEX
@@ -58,18 +58,25 @@
 #define QEI_SENSOR_POLARITY         1
 
 //////////////////////////////////////////////
+//////  PROTECTION CONFIGURATION
+////////////////////////////////////////////
+#define I_MAX           60      //maximum tolerable value of phase current (under abnormal conditions)
+#define V_DC_MAX        50      //maximum tolerable value of dc-bus voltage (under abnormal conditions)
+#define V_DC_MIN        10      //minimum tolerable value of dc-bus voltave (under abnormal conditions)
+#define TEMP_BOARD_MAX  100     //maximum tolerable value of board temperature (optional)
+
+//////////////////////////////////////////////
 //////  MOTOR COMMUTATION CONFIGURATION
 ////////////////////////////////////////////
 
 // COMMUTATION LOOP PERIOD (if applicable) [us]
-#define COMMUTATION_LOOP_PERIOD     110
-#define COMMUTATION_PERIOD          COMMUTATION_LOOP_PERIOD
+#define COMMUTATION_LOOP_PERIOD     66
 
 // COMMUTATION CW SPIN OFFSET (if applicable) [0:4095]
-#define COMMUTATION_OFFSET_CLK      0
+#define COMMUTATION_OFFSET_CLK      3450
 
 // COMMUTATION CCW SPIN OFFSET (if applicable) [0:4095]
-#define COMMUTATION_OFFSET_CCLK     342
+#define COMMUTATION_OFFSET_CCLK     0
 
 ///////////////////////////////////////////////
 //////  MOTOR CONTROL CONFIGURATION
@@ -79,7 +86,7 @@
 #define CONTROL_LOOP_PERIOD     1000
 
 // PID FOR POSITION CONTROL (if applicable) [will be divided by 10000]
-#define POSITION_Kp       1000
+#define POSITION_Kp       100
 #define POSITION_Ki       1
 #define POSITION_Kd       0
 
@@ -109,9 +116,9 @@
 // PROFILER LIMITIS
 #define MAX_POSITION_LIMIT      0x7fffffff        // ticks (max range: 2^30, limited for safe operation)
 #define MIN_POSITION_LIMIT      -0x7fffffff       // ticks (min range: -2^30, limited for safe operation)
-#define MAX_VELOCITY            4000            // rpm
-#define MAX_ACCELERATION        4000            // rpm/s
-#define MAX_DECELERATION        4000            // rpm/s
+#define MAX_VELOCITY            7000            // rpm
+#define MAX_ACCELERATION        7000            // rpm/s
+#define MAX_DECELERATION        7000            // rpm/s
 #define MAX_CURRENT_VARIATION   800             // adc_ticks/s
 #define MAX_CURRENT             800             // adc_ticks
 

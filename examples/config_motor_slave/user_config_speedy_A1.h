9716307a4c463b9fd00acd6b5be61b9f05ee26e4
Frank Jeschke <fjeschke@synapticon.com>
2016-06-12 22:15:24 +0200

    Add object index of objects in the object dictionary

--- examples/config_motor_slave/user_config_speedy_A1.h
+++ examples/config_motor_slave/user_config_speedy_A1.h
@@ -17,31 +17,31 @@
 //#define MOTOR_TYPE  BLDC_MOTOR
 
 // NUMBER OF POLE PAIRS (if applicable)
-#define POLE_PAIRS  15
+#define POLE_PAIRS  15               /* 0x2410:3 */
 
 // POLARITY OF THE MOVEMENT OF YOUR MOTOR [1,-1]
-#define POLARITY           1
+#define POLARITY           1      /* 0x607E */
 
 //equivalent to torque constant (Nm at 1000 A)
-#define MAXIMUM_TORQUE          57
+#define MAXIMUM_TORQUE          57   /* 0x20F6:6 (Motor Specific Torque Constant */
 
 // (uOhm)
-#define PHASE_RESISTANCE        552000
+#define PHASE_RESISTANCE        552000   /* 0x2410:2 */
 
 // (uH)
-#define PHASE_INDUCTANCE        720
+#define PHASE_INDUCTANCE        720      /* 0x2410:5 */
 
 // SENSOR USED FOR COMMUTATION (if applicable) [BISS_SENSOR - 4, CONTELEC_SENSOR]
-#define MOTOR_COMMUTATION_SENSOR   BISS_SENSOR
+#define MOTOR_COMMUTATION_SENSOR   BISS_SENSOR    /* 0x606A */
 
 // SENSOR USED FOR CONTROL FEEDBACK [BISS_SENSOR, CONTELEC_SENSOR]
 //#define MOTOR_FEEDBACK_SENSOR      BISS_SENSOR
 
 // RESOLUTION OF YOUR ENCODER (increments)
-#define SENSOR_RESOLUTION      262144 //0x40000  //!4000
+#define SENSOR_RESOLUTION      262144 //0x40000  //!4000 /* 0x308f */
 
 // POLARITY OF YOUR ENCODER [1, -1]
-#define SENSOR_POLARITY         1
+#define SENSOR_POLARITY         1      /* 0x2004 */
 
 
 ///////////////////////////////////////////////
@@ -52,19 +52,19 @@
 #define CONTROL_LOOP_PERIOD     1000
 
 // PID FOR POSITION CONTROL (units * 10000)
-#define POSITION_Kp       35
-#define POSITION_Ki       20
-#define POSITION_Kd       0
+#define POSITION_Kp       35   /* 0x20f9:1 */
+#define POSITION_Ki       20   /* 0x20f9:2 */
+#define POSITION_Kd       0    /* 0x20f9:3 */
 
 // PID FOR VELOCITY CONTROL (units * 10000)
-#define VELOCITY_Kp       30
-#define VELOCITY_Ki       0
-#define VELOCITY_Kd       40
+#define VELOCITY_Kp       30   /* 0x20fb:1 */
+#define VELOCITY_Ki       0    /* 0x20fb:2 */
+#define VELOCITY_Kd       40   /* 0x20fb:3 */
 
 // PID FOR TORQUE CONTROL (units * 10000)
-#define TORQUE_Kp         1000
-//#define TORQUE_Ki         0
-//#define TORQUE_Kd         0
+#define TORQUE_Kp         1000    /* 0x20f6:1 */
+//#define TORQUE_Ki         0     /* 0x20f6:2 */
+//#define TORQUE_Kd         0     /* 0x20f6:3 */
 
 
 //////////////////////////////////////////////
@@ -75,7 +75,7 @@
 #define COMMUTATION_LOOP_PERIOD     66
 
 // COMMUTATION CW SPIN OFFSET (if applicable) [0:4095]
-#define COMMUTATION_OFFSET_CLK      2071
+#define COMMUTATION_OFFSET_CLK      2071    /* 0x2001 */
 
 // COMMUTATION CCW SPIN OFFSET (if applicable) [0:4095]
 #define COMMUTATION_OFFSET_CCLK     0
@@ -85,7 +85,7 @@
 ////////////////////////////////////////////
 
 //maximum tolerable value of phase current (A)
-#define I_MAX           60
+#define I_MAX           60      /* 0x6073 */
 
 //maximum tolerable value of dc-bus voltage (V)
 #define V_DC_MAX        60
@@ -97,23 +97,23 @@
 //#define TEMP_BOARD_MAX  100
 
 // min range: -2^30, limited for safe operation (increments)
-#define MIN_POSITION_LIMIT      -0x7fffffff
+#define MIN_POSITION_LIMIT      -0x7fffffff    /* 0x607B:1 */
 
 // max range: 2^30, limited for safe operation (increments)
-#define MAX_POSITION_LIMIT      0x7fffffff
+#define MAX_POSITION_LIMIT      0x7fffffff    /* 0x607B:2 */
 
 // (1/min)
-#define MAX_VELOCITY             1300 //!200
+#define MAX_SPEED               1300 //!200  /* now 0x2410:4 future: 0x607F */
 
 // rpm/s
-#define QUICK_STOP_DECELERATION  3000
+#define QUICK_STOP_DECELERATION  3000       /* 0x6085 (future use) */
 
 // rpm/s
-#define MAX_ACCELERATION         3000
-#define MAX_DECELERATION         3000
+#define MAX_ACCELERATION         3000   /* 0x6083 (future use) */
+#define MAX_DECELERATION         3000   /* 0x6085 (future use) */
 
 // torque controller input limit (units * 1024)
-#define TORQUE_CONTROL_LIMIT    1200000 //!1000000
+#define TORQUE_CONTROL_LIMIT    1200000 //!1000000       /* 0x6072 */
 
 
 //////////////////////////////////////////////

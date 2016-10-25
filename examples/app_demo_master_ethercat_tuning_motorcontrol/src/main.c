eba6ba306b07978c3c7bd1bd932c2340dd9cf92b
Romuald Junot <rjunot@synapticon.com>
2016-10-25 20:16:00 +0200

    ethercat tuning: improve display and add emergency stop command

--- examples/app_demo_master_ethercat_tuning_motorcontrol/src/main.c
+++ examples/app_demo_master_ethercat_tuning_motorcontrol/src/main.c
@@ -29,6 +29,11 @@ typedef struct {
     int max_torque;
 } InputValues;
 
+typedef struct {
+    int last_command;
+    int last_value;
+} OutputValues;
+
 int r,c, // current row and column (upper-left is (0,0))
 nrows, // number of rows in window
 ncols; // number of columns in window
@@ -48,7 +53,7 @@ ncols; // number of columns in window
 //printf("Operation Mode disp: %d\n", slv_handles[slave_number].operation_mode_disp);
 
 #define OPMODE_TUNING    (-128)
-#define DISPLAY_LINE 15
+#define DISPLAY_LINE 17
 
 void draw(char dc)
 {
@@ -87,6 +92,8 @@ int main(int argc, char *argv[])
     clear(); // curses call to clear screen, send cursor to position (0,0)
     refresh(); // curses call to implement all changes since last refresh
     nodelay(stdscr, TRUE); //no delay
+//    start_color();           /* Start color          */
+//    init_pair(1, COLOR_RED, -1);
 
     r = DISPLAY_LINE; c = 0;
     int i = 0;
@@ -101,6 +108,7 @@ int main(int argc, char *argv[])
     int motor_polarity = 0, sensor_polarity = 0, torque_control_flag = 0, position_ctrl_flag = 0, brake_flag = 0;
     TuningMotorCtrlStatus motorctrl_status = TUNING_MOTORCTRL_OFF;
     InputValues input = {0};
+    OutputValues output = {0};
     int pole_pairs = 0;
     int target_position = 0;
     int target = 0;
@@ -134,16 +142,20 @@ int main(int argc, char *argv[])
     slv_handles[slave_number].motorctrl_out = 0;  //reset control word
 
     //init prompt
-    move(DISPLAY_LINE-6, 0);
+    move(DISPLAY_LINE-8, 0);
     printw("Commands:");
+    move(DISPLAY_LINE-7, 0);
+    printw("b: Release/Block Brake       | a: find offset (also release the brake)");
+    move(DISPLAY_LINE-6, 0);
+    printw("number: set torque command   | r: reverse torque command");
     move(DISPLAY_LINE-5, 0);
-    printw("b - Release/Block Brake ; a - find offset");
+    printw("ep3: enable position control | p + number: set position command");
     move(DISPLAY_LINE-4, 0);
-    printw("num value - torque command ; r - reverse torque command");
+    printw("P + number: set pole pairs");
     move(DISPLAY_LINE-3, 0);
-    printw("ep3 - enable position control ; p + num value - position command");
+    printw("L s/t/p + number: set speed/torque/position limit");
     move(DISPLAY_LINE-2, 0);
-    printw("L s/t/p + value - set speed/torque/position limit");
+    printw("** Double press Enter for emergency stop **");
     move(DISPLAY_LINE, 0);
     printw("> ");
     c=2;
@@ -204,63 +216,14 @@ int main(int argc, char *argv[])
             //print
             int line = 0;
             //row 0
-            move(line, 0);
-            clrtoeol();
-            printw("Position %14d | Velocity %4d",  slv_handles[slave_number].position_in, slv_handles[slave_number].speed_in);
-            line++;
-            //row 1
-            move(line, 0);
-            clrtoeol();
-            printw("Torque computed %4d    | Torque sensor %d", slv_handles[slave_number].torque_in, slv_handles[slave_number].user1_in);
-//            printw("controlword %4d    | statusword %d", slv_handles[slave_number].motorctrl_out & 0xff, statusword);
-            line++;
-            //row 2
-            move(line, 0);
-            clrtoeol();
-            printw("Offset %4d             | Pole pairs %2d", offset, pole_pairs);
-            line++;
-            //row 3
-            move(line,0);
-            clrtoeol();
-            if (motor_polarity == 0)
-                printw("Motor polarity normal   | ");
-            else
-                printw("Motor polarity inverted | ");
-            if (sensor_polarity == 0)
-                printw("Sensor polarity normal");
-            else
-                printw("Sensor polarity inverted");
-            line++;
-            //row 4
-            move(line,0);
-            clrtoeol();
-            if (torque_control_flag == 0)
-                printw("Motor control off       | ");
-            else
-                printw("Motor control on        | ");
-            if (brake_flag == 0)
-                printw("Brake blocking");
-            else
-                printw("Brake released");
-            line++;
-            //row 5
-            move(line,0);
-            clrtoeol();
-            printw("Speed  limit %5d      | ", input.max_speed);
-            printw("Position min %d", input.min_position);
-            line++;
-            //row 6
-            move(line,0);
-            clrtoeol();
-            printw("Torque limit %5d      | ", input.max_torque);
-            printw("Position max %d", input.max_position);
-            line++;
-            //row 6
             move(line,0);
             clrtoeol();
             //motorcontrol mode
+//            attron(COLOR_PAIR(1));
+            printw("** Operation mode: ");
             switch(motorctrl_status) {
             case TUNING_MOTORCTRL_OFF:
+                printw("off");
                 break;
             case TUNING_MOTORCTRL_TORQUE:
                 printw("Torque control %5d", target);
@@ -272,6 +235,61 @@ int main(int argc, char *argv[])
                 printw("Velocity control %5d", target);
                 break;
             }
+            printw(" **");
+//            attroff(COLOR_PAIR(1));
+            line++;
+            //row 1
+            move(line, 0);
+            clrtoeol();
+            printw("Position %14d | Velocity %4d",  slv_handles[slave_number].position_in, slv_handles[slave_number].speed_in);
+            line++;
+            //row 2
+            move(line, 0);
+            clrtoeol();
+            printw("Torque computed %4d    | Torque sensor %d", slv_handles[slave_number].torque_in, slv_handles[slave_number].user1_in);
+//            printw("controlword %4d    | statusword %d", slv_handles[slave_number].motorctrl_out & 0xff, statusword);
+            line++;
+            //row 3
+            move(line, 0);
+            clrtoeol();
+            printw("Offset %4d             | Pole pairs %2d", offset, pole_pairs);
+            line++;
+            //row 4
+            move(line,0);
+            clrtoeol();
+            if (motor_polarity == 0)
+                printw("Motor polarity normal   | ");
+            else
+                printw("Motor polarity inverted | ");
+            if (sensor_polarity == 0)
+                printw("Sensor polarity normal");
+            else
+                printw("Sensor polarity inverted");
+            line++;
+            //row 5
+            move(line,0);
+            clrtoeol();
+            if (torque_control_flag == 0)
+                printw("Motor control off       | ");
+            else
+                printw("Motor control on        | ");
+            if (brake_flag == 0)
+                printw("Brake blocking");
+            else
+                printw("Brake released");
+            line++;
+            //row 6
+            move(line,0);
+            clrtoeol();
+            printw("Speed  limit %5d      | ", input.max_speed);
+            printw("Position min %d", input.min_position);
+            line++;
+            //row 7
+            move(line,0);
+            clrtoeol();
+            printw("Torque limit %5d      | ", input.max_torque);
+            printw("Position max %d", input.max_position);
+            line++;
             move(DISPLAY_LINE, c);
         }
 
@@ -319,6 +337,13 @@ int main(int argc, char *argv[])
                 controlword = ((mode_2 & 0xff) << 8) | (mode & 0xff);
                 slv_handles[slave_number].motorctrl_out = controlword;
                 slv_handles[slave_number].user4_out         = mode_3 & 0xff;
+                //check for emergency stop
+                if (output.last_command == '@' && output.last_value == 0 && value == 0 && mode == '@') {
+                    slv_handles[slave_number].motorctrl_out = 'e';
+                    slv_handles[slave_number].user3_out = 0;
+                }
+                output.last_command = mode;
+                output.last_value = value;
                 value = 0;
                 mode = '@';
                 mode_2 = '@';

b384ac49b6a598d818e3fe7811b687472cc53289
Henrik Strötgen <hstroetgen@synapticon.com>
2016-12-15 18:15:08 +0100

    move file from src to include

---
+++ module_canopen_interface/include/canopen_service.h
@@ -0,0 +1,17 @@
+/*
+ * canopen_service.h
+ *
+ *  Created on: 14.12.2016
+ *      Author: hstroetgen
+ */
+
+
+#ifndef CANOPEN_SERVICE_H_
+#define CANOPEN_SERVICE_H_
+
+#include "pdo_interface.h"
+#include "od_interface.h"
+
+void canopen_service(server interface ODCommunicationInterface i_od[3], server interface PDOCommunicationInterface i_pdo[3]);
+
+#endif /* CANOPEN_SERVICE_H_ */

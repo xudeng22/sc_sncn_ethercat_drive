8395189bffa8cf220f9836b8cc6b3783cd150257
Henrik Strötgen <hstroetgen@synapticon.com>
2017-01-17 14:41:13 +0100

    add function header

--- module_canopen_interface/include/canopen_service.h
+++ module_canopen_interface/include/canopen_service.h
@@ -14,6 +14,10 @@
 #include "canod_datatypes.h"
 #include "pdo_handler.h"
 
+/**
+ * @brief Provides a service, which managed the object dictionary communication and stores all necessary values.
+ * @param i_od  Interface for setting and getting OD values.
+ */
 void canopen_service(server interface ODCommunicationInterface i_od[3]);
 
 #endif /* CANOPEN_SERVICE_H_ */

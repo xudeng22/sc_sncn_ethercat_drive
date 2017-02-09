10b471f781dc3412f74fc0674a862325aeb6f020
Frank Jeschke <fjeschke@synapticon.com>
2017-02-09 18:54:13 +0100

    Rework tokenizer to use a separate struct

--- deviceconfig.c
+++ deviceconfig.c
@@ -16,6 +16,11 @@
 #define MAX_INPUT_LINE    1024
 #define MAX_TOKEN_SIZE    255
 
+struct _token_t {
+	char **token;
+	size_t count;
+};
+
 static size_t get_token_count(char *buf, size_t bufsize)
 {
 	size_t separator = 0;
@@ -29,35 +34,34 @@ static size_t get_token_count(char *buf, size_t bufsize)
 	return (separator + 1);
 }
 
-static void dc_tokenize_inbuf(char *buf, size_t bufsize, char **token, size_t *token_count)
+static void dc_tokenize_inbuf(char *buf, size_t bufsize, struct _token_t *token)
 {
 	char *sep = ",";
 	char *b = malloc(bufsize * sizeof(char));
 	char *word = NULL;
 
 	size_t tokenitem = 0;
-	*token_count = get_token_count(buf, bufsize);
-	token = malloc(*token_count);
+	token->count = get_token_count(buf, bufsize);
+	token->token = malloc(token->count * sizeof(char *));
 
 	memmove(b, buf, bufsize * sizeof(char));
 
 	for (word = strtok(b, sep);  word; word = strtok(NULL, sep)) {
-		*(token + tokenitem) = malloc(strlen(word) + 1);
-		strncpy(*(token + tokenitem), word, strlen(word));
+		*(token->token + tokenitem) = malloc(strlen(word) + 1);
+		strncpy(*(token->token + tokenitem), word, (strlen(word) + 1));
 		tokenitem++;
 	}
-
 
 	free(b);
 }
 
-static void dc_parse_tokens(char **token, size_t token_count, SdoParam_t **params)
+static void dc_parse_tokens(struct _token_t *token, SdoParam_t **params)
 {
 	(void)params;
 
 	printf("[DEBUG tokens] ");
-	for (size_t i = 0; i < token_count; i++) {
-		printf("'%s', ", *(token + i));
+	for (size_t i = 0; i < token->count; i++) {
+		printf("'%s', ", *(token->token + i));
 	}
 	printf("\n");
 }
@@ -69,8 +73,9 @@ int dc_read_file(const char *path, SdoParam_t **params)
 		return -1;
 	}
 
-	char **token = NULL;
-	size_t token_count = 0;
+	struct _token_t token;
+	token.token = NULL;
+	token.count = 0;
 
 	char inbuf[MAX_INPUT_LINE];
 	size_t inbuf_length = 0;
@@ -86,8 +91,14 @@ int dc_read_file(const char *path, SdoParam_t **params)
 		if (c == '\n') {
 			if (inbuf_length > 1) {
 				inbuf[inbuf_length++] = '\0';
-				dc_tokenize_inbuf(inbuf, inbuf_length, token, &token_count);
-				dc_parse_tokens(token, token_count, params);
+				dc_tokenize_inbuf(inbuf, inbuf_length, &token);
+				dc_parse_tokens(&token, params);
+
+				/* FIXME find out why this free's don't work */
+				for (size_t k = 0; k < token.count; k++) {
+					//free(*(token.token + k));
+					//free(token.token);
+				}
 			}
 
 			inbuf_length = 0;

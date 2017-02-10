fb7f6c0c8e3acddba9b3e197a847cf3371294790
Frank Jeschke <fjeschke@synapticon.com>
2017-02-10 17:45:24 +0100

    Add print out of parameters
    This main function demonstrates the usage of the parser function and
    show the parsed values.

--- main.c
+++ main.c
@@ -16,5 +16,29 @@ int main(int argc, char *argv[])
             return -1;
     }
 
+	printf("Node Count = %lu; Parameter Count = %lu\n",
+                    config_parameter.node_count, config_parameter.param_count);
+
+    SdoParam_t **paramlist = config_parameter.parameter;
+    if (paramlist == NULL) {
+            fprintf(stderr, "Error list of parameter per nodes is empty\n");
+            return -1;
+    }
+
+    for (size_t i = 0; i < config_parameter.node_count; i++) {
+            SdoParam_t *nparam = *paramlist + i;
+            if (nparam == NULL) {
+                    fprintf(stderr, "Error parameter list of node is empty\n");
+                    return -1;
+            }
+
+            for (size_t k = 0; k < config_parameter.param_count; k++) {
+                    SdoParam_t *p = nparam + k;
+
+                    printf("N%zu: 0x%04x:%d = %d\n",
+                                    i, p->index, p->subindex, p->value);
+            }
+    }
+
     return 0;
 }

d13d1d5653b5e25155994c142db0123e9cb58897
Frank Jeschke <fjeschke@synapticon.com>
2017-02-11 12:00:17 +0100

    Apply sncn coding style

--- deviceconfig.h
+++ deviceconfig.h
@@ -15,16 +15,19 @@
 #include <stdlib.h>
 
 typedef struct {
-    uint16_t index;
-    uint8_t  subindex;
-    uint32_t value;      /* FIXME hold values of arbitrary size! */
-    size_t   bytecount;  /* for this value I need access to the object dictionary! FIXME may remove from here! */
+  uint16_t index;
+  uint8_t  subindex;
+  uint32_t value;      /* FIXME hold values of arbitrary size! */
+  size_t   bytecount;  /* for this value I need access to the object dictionary! FIXME may remove from here! */
 } SdoParam_t;
 
 typedef struct {
-    size_t     node_count;   ///< Number of nodes in the config parameters file, this value needs to be checked against the real number of nodes on the bus
-    size_t     param_count;  ///< Number of configuration parameters, aka non commented lines in the config file
-    SdoParam_t **parameter;  ///< array of node_count x param_count of configuration parameters, for every node is a list of SdoParam_t objects.
+  size_t
+  node_count;   ///< Number of nodes in the config parameters file, this value needs to be checked against the real number of nodes on the bus
+  size_t
+  param_count;  ///< Number of configuration parameters, aka non commented lines in the config file
+  SdoParam_t
+  **parameter;  ///< array of node_count x param_count of configuration parameters, for every node is a list of SdoParam_t objects.
 } SdoConfigParameter_t;
 
 int dc_read_file(const char *path, SdoConfigParameter_t *params);
--- main.c
+++ main.c
@@ -4,41 +4,41 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2) {
-        printf("Usage: %s <filename>\n", argv[0]);
-        return -1;
+  if (argc < 2) {
+    printf("Usage: %s <filename>\n", argv[0]);
+    return -1;
+  }
+
+  SdoConfigParameter_t config_parameter = { 0, 0, NULL };
+
+  if (dc_read_file(argv[1], &config_parameter) != 0) {
+    fprintf(stderr, "Error parsing file\n");
+    return -1;
+  }
+
+  printf("Node Count = %lu; Parameter Count = %lu\n",
+         config_parameter.node_count, config_parameter.param_count);
+
+  SdoParam_t **paramlist = config_parameter.parameter;
+  if (paramlist == NULL) {
+    fprintf(stderr, "Error list of parameter per nodes is empty\n");
+    return -1;
+  }
+
+  for (size_t i = 0; i < config_parameter.node_count; i++) {
+    SdoParam_t *nparam = *paramlist + i;
+    if (nparam == NULL) {
+      fprintf(stderr, "Error parameter list of node is empty\n");
+      return -1;
     }
 
-    SdoConfigParameter_t config_parameter = { 0, 0, NULL };
+    for (size_t k = 0; k < config_parameter.param_count; k++) {
+      SdoParam_t *p = nparam + k;
 
-    if (dc_read_file(argv[1], &config_parameter) != 0) {
-            fprintf(stderr, "Error parsing file\n");
-            return -1;
+      printf("N%zu: 0x%04x:%d = %d\n",
+             i, p->index, p->subindex, p->value);
     }
+  }
 
-	printf("Node Count = %lu; Parameter Count = %lu\n",
-                    config_parameter.node_count, config_parameter.param_count);
-
-    SdoParam_t **paramlist = config_parameter.parameter;
-    if (paramlist == NULL) {
-            fprintf(stderr, "Error list of parameter per nodes is empty\n");
-            return -1;
-    }
-
-    for (size_t i = 0; i < config_parameter.node_count; i++) {
-            SdoParam_t *nparam = *paramlist + i;
-            if (nparam == NULL) {
-                    fprintf(stderr, "Error parameter list of node is empty\n");
-                    return -1;
-            }
-
-            for (size_t k = 0; k < config_parameter.param_count; k++) {
-                    SdoParam_t *p = nparam + k;
-
-                    printf("N%zu: 0x%04x:%d = %d\n",
-                                    i, p->index, p->subindex, p->value);
-            }
-    }
-
-    return 0;
+  return 0;
 }

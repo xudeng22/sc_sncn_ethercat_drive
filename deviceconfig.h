896eec55034044759576a15fc7e0eae35fd1fb2a
Frank Jeschke <fjeschke@synapticon.com>
2017-02-11 12:20:48 +0100

    Add documentation

--- deviceconfig.h
+++ deviceconfig.h
@@ -14,24 +14,29 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+/**
+ * \brief Structure describing a individual parameter
+ */
 typedef struct {
-  uint16_t index;
-  uint8_t  subindex;
-  uint32_t value;      /* FIXME hold values of arbitrary size! */
+  uint16_t index;      ///< Index of the associated object in the object dictionary
+  uint8_t  subindex;   ///< Subindex of this object
+  uint32_t value;      ///< Value of the container /* FIXME hold values of arbitrary size! */
   size_t   bytecount;  /* for this value I need access to the object dictionary! FIXME may remove from here! */
 } SdoParam_t;
 
 typedef struct {
-  size_t
-  node_count;   ///< Number of nodes in the config parameters file, this value needs to be checked against the real number of nodes on the bus
-  size_t
-  param_count;  ///< Number of configuration parameters, aka non commented lines in the config file
-  SdoParam_t
-  **parameter;  ///< array of node_count x param_count of configuration parameters, for every node is a list of SdoParam_t objects.
+  size_t node_count;       ///< Number of nodes in the config parameters file, this value needs to be checked against the real number of nodes on the bus
+  size_t param_count;      ///< Number of configuration parameters, aka non commented lines in the config file
+  SdoParam_t **parameter;  ///< array of node_count x param_count of configuration parameters, for every node is a list of SdoParam_t objects.
 } SdoConfigParameter_t;
 
+/**
+ * \brief Function to read configuration file and parse the content
+ *
+ * \param path     Filename with full path of the file to process
+ * \param params   Pointer to a \c SdoConfigParameter_t object
+ * \return         0 if no error
+ */
 int dc_read_file(const char *path, SdoConfigParameter_t *params);
-
-void dc_release(void);
 
 #endif /* DEVICECONFIG_H */
--- main.c
+++ main.c
@@ -11,14 +11,33 @@ int main(int argc, char *argv[])
 
   SdoConfigParameter_t config_parameter = { 0, 0, NULL };
 
+  /*
+   * Read the device configurations from file `filename`
+   */
   if (dc_read_file(argv[1], &config_parameter) != 0) {
     fprintf(stderr, "Error parsing file\n");
     return -1;
   }
 
+  /*
+   * Get the node count and the parameter count.
+   * This configuration is intended for motordrives only, this means the file
+   * parser will fail if the number of parameter objects differ between nodes.
+   *
+   * To configure a network with different nodes the user of this module needs
+   * to take care of this.
+   */
   printf("Node Count = %lu; Parameter Count = %lu\n",
          config_parameter.node_count, config_parameter.param_count);
 
+  /*
+   * Get the reference of the parameter list to simplify handling.
+   * The parameter list is a 2 dimensional array with dimensions
+   *
+   *   number of nodes X number of parameters
+   *
+   * The individual nodes are not mentioned separately.
+   */
   SdoParam_t **paramlist = config_parameter.parameter;
   if (paramlist == NULL) {
     fprintf(stderr, "Error list of parameter per nodes is empty\n");

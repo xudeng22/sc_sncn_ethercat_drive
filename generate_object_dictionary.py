41fc7a0970f822cc1dcc9d2b9da1893e83daa3c1
Frank Jeschke <fjeschke@synapticon.com>
2017-06-06 16:53:14 +0200

    Add data structures for objects
    A entry class and various enum classes are added to make the parsed
    values more organized.

--- generate_object_dictionary.py
+++ generate_object_dictionary.py
@@ -7,6 +7,21 @@ EsiFile = './SOMANET_CiA_402.xml'
 
 
 # object type enumeration
+
+# currently no destinction in which state we are
+class Access(Enum):
+    RO = 0
+    RW = 1
+    WO = 2
+
+
+class PdoMap(Enum):
+    NONE = 0
+    RXPDO = 1
+    TXPDO = 2
+    RXTXPDO = 3
+
+
 class ObjectType(Enum):
     UNKNOWN = 0
     VAR = 1
@@ -14,17 +29,47 @@ class ObjectType(Enum):
     RECORD = 3
 
 
+class DataType(Enum):
+    UNKNOWN = 0
+    SINT = 1
+    USINT = 2
+    INT = 3
+    UINT = 3
+    DINT = 4
+    UDINT = 5
+
+
+class entry(object):
+    subindex = 0  # kind of redundant
+    value = 0
+    max_value = 0
+    min_value = 0
+    default = 0
+    dataType = DataType.UNKNOWN
+    accessRights = Access.RW
+    pdoMappable = PdoMap.NONE
+    bitsize = 0
+    name = ""
+
+
 class object(object):
     index = 0
     otype = ObjectType.UNKNOWN
+    entry_count = 0
+    entry = []
+    name = ""
 
     def __init__(self, index, name, otype):
         self.name = name
         self.index = index
         self.otype = otype
 
+    def add_entry(entries):
+        entry.append(entries)          # list of entries of class entries
+        entry_count = entries.length
+
     def print_ccode():
-	print "struct _object object_"
+        print "struct _object object_"
 
 # parse the ESI
 
